40f99395bd50dfdd753cd8299a3179f31556be25
princewill <princewillolaiya@gmail.com>
2021-02-03 06:53:25 +0100

    separated secret key from settings.py

--- management/settings.py
+++ management/settings.py
@@ -11,6 +11,7 @@ https://docs.djangoproject.com/en/2.2/ref/settings/
 """
 
 import os
+from .secret import SECRET_KEY
 
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
@@ -20,7 +21,6 @@ BASE_DIR = os.path.dirname(os.path.dirname(os.path.abspath(__file__)))
 # See https://docs.djangoproject.com/en/2.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = '*k3fw(v!&b0h-e_a4+v&zdc(v=b(%0b=j49mczft-n%7kw^a)l'
 
 # SECURITY WARNING: don't run with debug turned on in production!
 DEBUG = True
